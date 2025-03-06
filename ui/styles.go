3ffa110e50298e32116ed02ddece1dfbf638ab62
Derian Escalante <deriannavy@gmail.com>
2025-03-05 21:43:17 -0600

    ajuste color

--- ui/styles.go
+++ ui/styles.go
@@ -63,14 +63,14 @@ func DefaultItemStyle() ItemStyle {
 		SelectedTitle: lipgloss.NewStyle().Bold(true).Foreground(lipgloss.AdaptiveColor{Light: "#F793FF", Dark: "#ffffff"}).Padding(0, 1, 0, 1),
 
 		// Cursor Style
-		SelectedCursor: lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Padding(0, 0, 0, 1),
+		SelectedCursor: lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#750404"}).Padding(0, 0, 0, 1),
 
 		// Methods Style
 		GetMethod:     lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#68D696"}).Padding(0, 0, 0, 1),
 		PostMethod:    lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#F5DB7B"}).Padding(0, 0, 0, 1),
 		PutMethod:     lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#486385"}).Padding(0, 0, 0, 1),
 		PatchMethod:   lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#BEA6DE"}).Padding(0, 0, 0, 1),
-		DeleteMethod:  lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#F2958D"}).Padding(0, 0, 0, 1),
+		DeleteMethod:  lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#BC4B47"}).Padding(0, 0, 0, 1),
 		OptionsMethod: lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#D959AC"}).Padding(0, 0, 0, 1),
 		HeadMethod:    lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#68D696"}).Padding(0, 0, 0, 1),
 		UnknowMethod:  lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#999999"}).Padding(0, 0, 0, 1),
@@ -112,6 +112,6 @@ func DefaultTabsStyle() TabsStyle {
 		NormalBorderTitle:   lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#aaaaaa"}).Padding(0, 0, 0, 1), //nolint:mnd
 		SelectedBorderTitle: lipgloss.NewStyle().Bold(true).Foreground(lipgloss.AdaptiveColor{Light: "#F793FF", Dark: "#ffffff"}).Padding(0, 0, 0, 1),
 		// Cursor Style
-		SelectedCursor: lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Padding(0, 0, 0, 1),
+		SelectedCursor: lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#1a1a1a", Dark: "#750404"}).Padding(0, 0, 0, 1),
 	}
 }
